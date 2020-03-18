07bb66c9792d6ba62f00bdfc5cc262bd306e218d
princebansal42 <princebansal42@gmail.com>
2020-03-18 19:40:11 +0530

    Project Structure Added and Auth Middleware setup

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const app = express();
-const connectDB = require("./config/db");
 
 // Connecting Database
+const connectDB = require("./config/db");
 connectDB();
 
 app.use(express.json({ extended: false }));
+
+// Routes Defined here
+const routes = require("./routes");
+app.use(routes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("Server running at " + PORT));

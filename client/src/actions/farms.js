a6595a1a26590f4955d6528ac114d57bace5fffb
princebansal42 <princebansal42@gmail.com>
2020-04-16 20:02:37 +0530

    Farm Action and Reducers Added

---
+++ client/src/actions/farms.js
@@ -0,0 +1,145 @@
+import axios from "axios";
+import {
+    FARM_LIST_REQUEST,
+    FARM_LIST_SUCCESS,
+    FARM_LIST_FAILURE,
+    FARM_REQUEST,
+    FARM_SUCCESS,
+    FARM_FAILURE,
+    FARM_ADD_REQUEST,
+    FARM_ADD_SUCCESS,
+    FARM_ADD_FAILURE,
+    FARM_EDIT_REQUEST,
+    FARM_EDIT_SUCCESS,
+    FARM_EDIT_FAILURE,
+    FARM_DELETE_REQUEST,
+    FARM_DELETE_SUCCESS,
+    FARM_DELETE_FAILURE,
+} from "./types";
+
+import setAuthToken from "../utils/setAuthToken";
+
+// Get Farms
+export const getFarmList = () => async (dispatch) => {
+    dispatch({
+        type: FARM_LIST_REQUEST,
+    });
+    try {
+        const res = await axios.get("/api/farms");
+        dispatch({
+            type: FARM_LIST_SUCCESS,
+            payload: res.data,
+        });
+    } catch (err) {
+        const errors = err.response.data.errors;
+        console.log(errors);
+        dispatch({
+            type: FARM_LIST_FAILURE,
+        });
+    }
+};
+
+// Get farm
+export const getFarm = (id) => async (dispatch) => {
+    dispatch({
+        type: FARM_REQUEST,
+    });
+    try {
+        const res = await axios.get(`/api/farms/${id}`);
+        dispatch({
+            type: FARM_SUCCESS,
+            payload: res.data,
+        });
+    } catch (err) {
+        const errors = err.response.data.errors;
+        console.log(errors);
+        dispatch({
+            type: FARM_FAILURE,
+        });
+    }
+};
+
+// Add Farm
+export const addFarm = (farmDetail) => async (dispatch) => {
+    const {
+        area,
+        location: { lat, long },
+        address,
+    } = farmDetail;
+    dispatch({
+        type: FARM_ADD_REQUEST,
+    });
+    const config = {
+        headers: {
+            "Content-Type": "application/json",
+        },
+    };
+
+    const body = JSON.stringify({ area, location: { lat, long }, address });
+    try {
+        const res = await axios.post("/api/farms", body, config);
+        dispatch({
+            type: FARM_ADD_SUCCESS,
+            payload: res.data,
+        });
+    } catch (err) {
+        const errors = err.response.data.errors;
+        console.log(errors);
+        dispatch({
+            type: FARM_ADD_FAILURE,
+        });
+    }
+};
+
+// Edit Farm
+export const editFarm = (id, newfarmDetail) => async (dispatch) => {
+    const {
+        area,
+        location: { lat, long },
+        address,
+    } = farmDetail;
+    dispatch({
+        type: FARM_EDIT_REQUEST,
+    });
+    const config = {
+        headers: {
+            "Content-Type": "application/json",
+        },
+    };
+
+    const body = JSON.stringify({ area, location: { lat, long }, address });
+    try {
+        const res = await axios.put(`/api/farms/${id}`, body, config);
+        dispatch({
+            type: FARM_EDIT_SUCCESS,
+            payload: res.data,
+        });
+    } catch (err) {
+        const errors = err.response.data.errors;
+        console.log(errors);
+        dispatch({
+            type: FARM_EDIT_FAILURE,
+        });
+    }
+};
+
+// Delete Farm
+export const deleteFarm = (id) => async (dispatch) => {
+    dispatch({
+        type: FARM_DELETE_REQUEST,
+    });
+
+    try {
+        const res = await axios.delete(`/api/farms/${id}`);
+        dispatch({
+            type: FARM_DELETE_SUCCESS,
+            payload: id,
+        });
+    } catch (err) {
+        const errors = err.response.data.errors;
+        console.log(errors);
+        dispatch({
+            type: FARM_DELETE_FAILURE,
+        });
+    }
+};
---
+++ client/src/reducers/farm.js
@@ -0,0 +1,104 @@
+import {
+    FARM_LIST_REQUEST,
+    FARM_LIST_SUCCESS,
+    FARM_LIST_FAILURE,
+    FARM_REQUEST,
+    FARM_SUCCESS,
+    FARM_FAILURE,
+    FARM_ADD_REQUEST,
+    FARM_ADD_SUCCESS,
+    FARM_ADD_FAILURE,
+    FARM_EDIT_REQUEST,
+    FARM_EDIT_SUCCESS,
+    FARM_EDIT_FAILURE,
+    FARM_DELETE_REQUEST,
+    FARM_DELETE_SUCCESS,
+    FARM_DELETE_FAILURE,
+} from "../actions/types";
+
+const initialState = {
+    farms: [],
+    farm: null,
+    loading: true,
+    error: {},
+};
+
+export default function (state = initialState, action) {
+    const { type, payload } = action;
+
+    switch (type) {
+        case FARM_LIST_REQUEST:
+        case FARM_REQUEST:
+        case FARM_ADD_REQUEST:
+        case FARM_EDIT_REQUEST:
+        case FARM_DELETE_REQUEST:
+            return {
+                ...state,
+                loading: true,
+            };
+        case FARM_LIST_SUCCESS:
+            return {
+                ...state,
+                farms: payload,
+                loading: false,
+            };
+
+        case FARM_LIST_FAILURE:
+            return {
+                ...state,
+                farms: [],
+                farm: null,
+                loading: false,
+            };
+
+        case FARM_SUCCESS:
+            return {
+                ...state,
+                farm: payload,
+                loading: false,
+            };
+
+        case FARM_ADD_SUCCESS:
+            return {
+                ...state,
+                farms: [...state.farms, { ...payload }],
+                farm: payload,
+                loading: false,
+            };
+
+        case FARM_DELETE_SUCCESS:
+            return {
+                ...state,
+                farms: state.farms.filter((farm) => farm._id !== payload.id),
+                farm: null,
+                loading: false,
+            };
+        case FARM_FAILURE:
+        case FARM_ADD_FAILURE:
+        case FARM_DELETE_FAILURE:
+            return {
+                ...state,
+                farm: null,
+                loading: false,
+            };
+
+        case FARM_EDIT_SUCCESS:
+            return {
+                ...state,
+                farms: state.farms.map((farm) => {
+                    if (farm._id === payload._id) return payload;
+                    return farm;
+                }),
+                farm: payload,
+                loading: false,
+            };
+        case FARM_EDIT_FAILURE:
+            return {
+                ...state,
+                loading: false,
+            };
+
+        default:
+            return state;
+    }
+}
